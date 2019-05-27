a1553ebf039687a4d89d7faaf17384eab944f797
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-27 09:55:15 +0300

    способы реализовать таймер в приложении ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
@@ -3,6 +3,7 @@ package com.omelchenkoaleks.cooltimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
 
@@ -14,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+            ТАЙМЕР
+         */
+
+        CountDownTimer myTimer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("myTimer", String.valueOf(millisUntilFinished/1000)
+                        + " seconds left");
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("myTimer", "Finish");
+            }
+        };
+
+        myTimer.start();
 
 
         /*
