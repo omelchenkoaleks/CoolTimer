500eefaba452e504719b4089705ebed01ec35ace
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-27 13:52:50 +0300

    CountDownTimer используем чтобы запустить таймер и создаем метод, который будет updateTimer() из указанного места ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -30,26 +31,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                // получаем минуты и секунды
-                int minutes = progress/60;
-                int seconds = progress - (minutes * 60);
-
-                String minutesString = "";
-                String secondsString = "";
-
-                if (minutes < 10) {
-                    minutesString = "0" + minutes;
-                } else {
-                    minutesString = String.valueOf(minutes);
-                }
-
-                if (seconds < 10) {
-                    secondsString = "0" + seconds;
-                } else {
-                    secondsString = String.valueOf(seconds);
-                }
-
-                mTextView.setText(minutesString + ":" + secondsString);
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
             }
 
             @Override
@@ -62,5 +45,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void start(View view) {
+        new CountDownTimer(mSeekBar.getProgress() * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("onFinish: ", "Finish");
+            }
+        }.start();
+    }
+
+    private void updateTimer(long millisUntilFinished) {
+        int minutes = (int) (millisUntilFinished/1000/60);
+        int seconds = (int) (millisUntilFinished/1000 - (minutes * 60));
+
+        String minutesString = "";
+        String secondsString = "";
+
+        if (minutes < 10) {
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = String.valueOf(minutes);
+        }
+
+        if (seconds < 10) {
+            secondsString = "0" + seconds;
+        } else {
+            secondsString = String.valueOf(seconds);
+        }
+
+        mTextView.setText(minutesString + ":" + secondsString);
     }
 }
