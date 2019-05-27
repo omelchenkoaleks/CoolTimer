170ad648c08b9bee0c23734d57af184ca8944b13
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-27 09:39:49 +0300

    Handler and Runnable - использовать как таймер ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
@@ -3,6 +3,10 @@ package com.omelchenkoaleks.cooltimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.concurrent.RunnableFuture;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /*
+            Способ выводить нужную информацию через заданное время
+            или как таймер ...
+         */
+//        final Handler handler = new Handler();
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d("Runnable: ", "Two seconds are passed");
+//                handler.postDelayed(this, 2000);
+//            }
+//        };
+//
+//        handler.post(runnable);
     }
 }
