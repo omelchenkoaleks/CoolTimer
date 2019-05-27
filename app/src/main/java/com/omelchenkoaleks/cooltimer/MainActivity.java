2276ac9d522b196272aa590414f69f96eaed19ec
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-27 13:25:31 +0300

    получаем минуты и секунды - привязываем слушатель к seekbar и вывод их в textview ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
@@ -6,14 +6,61 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.concurrent.RunnableFuture;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar mSeekBar;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = findViewById(R.id.seek_bar);
+        mTextView = findViewById(R.id.text_view);
+
+        mSeekBar.setMax(600);
+        mSeekBar.setProgress(60);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // получаем минуты и секунды
+                int minutes = progress/60;
+                int seconds = progress - (minutes * 60);
+
+                String minutesString = "";
+                String secondsString = "";
+
+                if (minutes < 10) {
+                    minutesString = "0" + minutes;
+                } else {
+                    minutesString = String.valueOf(minutes);
+                }
+
+                if (seconds < 10) {
+                    secondsString = "0" + seconds;
+                } else {
+                    secondsString = String.valueOf(seconds);
+                }
+
+                mTextView.setText(minutesString + ":" + secondsString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
