d91716dc0764250bcda23403dcd71b6e88d7e25c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-27 10:26:54 +0300

    моя верстка экрана по заданию ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/MainActivity.java
@@ -15,40 +15,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*
-            ТАЙМЕР
-         */
-
-        CountDownTimer myTimer = new CountDownTimer(10000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                Log.d("myTimer", String.valueOf(millisUntilFinished/1000)
-                        + " seconds left");
-            }
-
-            @Override
-            public void onFinish() {
-                Log.d("myTimer", "Finish");
-            }
-        };
-
-        myTimer.start();
-
-
-        /*
-            Способ выводить нужную информацию через заданное время
-            или как таймер ...
-         */
-//        final Handler handler = new Handler();
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//                Log.d("Runnable: ", "Two seconds are passed");
-//                handler.postDelayed(this, 2000);
-//            }
-//        };
-//
-//        handler.post(runnable);
     }
 }
