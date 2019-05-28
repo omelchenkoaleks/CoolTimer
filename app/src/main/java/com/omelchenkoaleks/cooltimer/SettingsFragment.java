ad2ccefe18b943425a2ba76c6438a1f09c2068a0
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-28 09:50:31 +0300

    код для вывода на экран в настройках, какая именно мелодия сейчас установлена и выбрана ...

--- app/src/main/java/com/omelchenkoaleks/cooltimer/SettingsFragment.java
+++ app/src/main/java/com/omelchenkoaleks/cooltimer/SettingsFragment.java
@@ -1,12 +1,74 @@
 package com.omelchenkoaleks.cooltimer;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.timer_preferences);
+
+        /*
+            весь этот код нужен для отображения в выборе настроек надписи, какая именно
+            мелодия включена на данный момент (под Timer Melody)
+         */
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+        // чтобы получить количество настроек, которые находятся
+        // на PreferenceScreen (timer_preference)
+        int count = preferenceScreen.getPreferenceCount();
+
+        // теперь нужно получить конкретную настройку - получаем ее при помощи итерации:
+        for (int i = 0; i < count; i++) {
+            Preference preference = preferenceScreen.getPreference(i);
+
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceLabel(preference, value);
+            }
+        }
+    }
+
+    //
+    private void setPreferenceLabel(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int index = listPreference.findIndexOfValue(value);
+            if (index >= 0) {
+                listPreference.setSummary(listPreference.getEntries()[index]);
+            }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (!(preference instanceof CheckBoxPreference)) {
+            String value = sharedPreferences.getString(preference.getKey(), "");
+            setPreferenceLabel(preference, value);
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // нужно зарегистрировать слушателя
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // нужно удалить из регистрации
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
